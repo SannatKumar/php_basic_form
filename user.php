086e415b62cd7b3af2628208c9ffe882fda1ee55
Raj Kumar <interplaytheheads@gmail.com>
2021-11-13 19:14:27 +0200

    php value accessed

---
+++ user.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<body>
+<?php
+
+$userName = $_POST["logname"];
+$password = $_POST["logpassword"];
+
+echo"<h1>".$userName."</h1>";
+echo"<h1>".$password."</h1>";
+
+?>
+
+    
+</body>
+</html>
+
+
+
